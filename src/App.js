6f37d985310f2cd3aeae21cb5ccb80324e15af68
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-10-26 19:07:49 +0300

    import the main libraries in App.hs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Box} from '@mui/material';
 
-const App = () => {
-    return (
-        <div>App</div>
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{background:'#000'}}>
+
+        </Box>
+    </BrowserRouter>
     )
-}
+
 
 export default App;

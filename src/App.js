c8e64f59e7c88aa6ab9aaa4598e9e58e4d80bc30
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-10-26 19:18:26 +0300

    add the routes inside App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import {Box} from '@mui/material';
 const App = () => (
     <BrowserRouter>
         <Box sx={{background:'#000'}}>
-
+            Navbar
+            {/* Routes is a built-in component by react router dom */}
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                {/* if we go to /video/123432 means we're going to go to a specific video page */}
+                <Route path="/video/:id" element={<VideoDetail />} />
+                {/* we can go to specific channel through /channel/1231  */}
+                <Route path="/channel/:id" element={<ChannelDetail />} />
+                {/* go to specific searchterm  */}
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
         </Box>
     </BrowserRouter>
     )

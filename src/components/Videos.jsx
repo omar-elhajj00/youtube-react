52071328539931a0a9961425519df20659b4a2dd
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-10-31 20:10:15 +0200

    start implementing the videos components

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {Stack , Box} from "@mui/material";
 
 const Videos = ({videos}) => {
-  console.log(videos);
   return (
     
-    <div>Videos</div>
+    <Stack 
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+      >
+        {videos.map((item, idx) => (
+          <Box key={idx}>
+            {/* if the item has an id and then the id points to the video id, that means that it is a video so we can say and and and then we want to render a videocard component */}
+            {item.id.videoId && <VideoCard video={item} /> }
+
+            {item.id.channelId && <ChannelCard channelDetail={item} /> }
+
+
+          </Box>
+        )
+
+        )}
+    </Stack>
   )
 }
 

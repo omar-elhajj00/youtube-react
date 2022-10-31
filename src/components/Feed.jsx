93b9d63ae004f973799f54936f73b61edd3dcb81
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-10-31 15:54:30 +0200

    create and import sideBar component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {Box, Stack, Typography} from '@mui/material';
+import SideBar from './SideBar';
+
+
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{
+        flexDirection: {sx: 'column',medium:'row' } //on medium devices and larger the flexDirection property is going to set to row 
+      }}
+    >
+      <Box 
+        sx={{
+          height: {sx: 'auto',md:'92vh'},
+          borderRight: '1px solid #3d3d3d',
+          px: {sx: 0, md: 2}
+        }}  
+      >
+        <SideBar />
+        <Typography
+          variant="body2"
+          sx={{
+            mt: 1.5,
+            color: '#fff'
+          }}
+          >
+          Copy Right 2022 Omar El Hajj
+        </Typography>
+
+      </Box>
+    
+    </Stack>    
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+
+import {Search} from '@mui/icons-material';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    // paper is a simple div has a white background and some elevation ,it appears like it's floating on the top
+    <Paper
+        component="form"
+        onSubmit={() => {}}
+        sx={{
+            borderRadius: 20,
+            border: '1px solid #e3e3e3',
+            pl: 2,
+            boxShadow: 'none',
+            mr: {sm: 5} //margin right only for small devices set to 5 ,this is phenomenal 
+        }}
+    >
+        <input 
+            className="search-bar"
+            type="text" 
+            value=""
+            onChange={() => {}}
+        />
+        <IconButton 
+            type="submit"
+            sx={{
+                p: '10px',
+                color: 'red',
+            }}
+        >
+            <Search />
+        </IconButton>
+        
+
+    </Paper>
   )
 }
 

59cba94adcaa2faec3e10a313cfcb637f606e8a4
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-10-31 16:37:52 +0200

    import videos components inside the feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Stack, Typography} from '@mui/material';
-import SideBar from './SideBar';
+import { SideBar, Videos} from './';
 
 
 
@@ -32,10 +32,11 @@ const Feed = () => {
 
       </Box>
 
-      <Box p={2}>
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
            New <span style={{color: '#f31503'}}>videos</span>
         </Typography>
+        <Videos />
       </Box>
     
     </Stack>    

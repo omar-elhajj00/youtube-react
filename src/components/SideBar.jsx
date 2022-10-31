245f8fbeaf65e2b5e8a90eece132448ddff917ad
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-10-31 16:01:18 +0200

    implement the sideBAr from the categories in utils/constants

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {Stack} from '@mui/material';
 
-const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
-}
+import categories from '../utils/constants';
+
+const SideBar = () => (
+
+    <Stack 
+        direction= "row"
+        sx= {{
+            overflowY: 'auto',
+            height: { sx: 'auto', md:'95%'},
+            flexDirection: { md: 'column'}
+        }}  
+        >
+            {categories.map((category) => {
+                <button>
+                    <span>
+                        {category.icon}
+                    </span>
+                    <span>
+                        {category.name}
+                    </span>
+                </button>
+            })}
+        
+    </Stack>
+
+)
 
 export default SideBar

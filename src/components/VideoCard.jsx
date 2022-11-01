cf57869af78f9ea05d473706cac984ce54d87a23
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-01 09:45:07 +0200

    design the width and height of every video card image insie videoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Card, CardMedia, CardContent} from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
-const VideoCard = () => {
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl } from '../utils/constants';
+const VideoCard = ({video: {id: {videoId}, snippet}}) => {
+  console.log(videoId, snippet);
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        {/* make sure to put these question marks before every dot because that way we're not going to get any errors, it's just going to be undefined */}
+        <CardMedia 
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{
+            width:358,
+            height:180 
+          }}
+          /> 
+      </Link>
+    </Card>
   )
 }
 

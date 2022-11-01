e0aca19f3e77b56ca8d51ee33d43e39c45739b1b
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-01 15:20:08 +0200

    style the image of cardContent inside channelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -13,8 +13,27 @@ const ChannelCard = ({channelDetail}) => (
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-      
-      </Link>ChannelCard</Box>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#fff'
+          }}
+        >
+          <CardMedia 
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: '50%',
+              height: '180px',
+              width: '180px'
+            }} />
+
+        </CardContent>
+      </Link>
+    </Box>
   )
 
 

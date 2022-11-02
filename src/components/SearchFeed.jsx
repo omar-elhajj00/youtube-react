147b31826f5db3245a8e983d63ac7a242238b528
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-02 22:37:59 +0200

    add the useParams into searchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Typography} from '@mui/material';
+import { useParams } from 'react-router-dom';
 
 import { Videos} from './';
 import {fetchFromAPI} from '../utils/fetchFromAPI';
@@ -9,16 +10,17 @@ import {fetchFromAPI} from '../utils/fetchFromAPI';
 const SearchFeed = () => {
 
   const [videos, setVideos] = useState([]);
+  const {searchTerm} = useParams();
 
   // useEffect is a lifecycle hook in which the code in this function will only run when we change the second parameter 
   useEffect(()=>{
-    fetchFromAPI(`search?part=snippet&q=`)//all the string inside `` are called template string
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)//all the string inside `` are called template string
       .then((data) => {
         console.log(data);
         setVideos(data.items)
       } ) //.then contain what will be result of the promise
        
-  },[]);//recall the fetchFromApi function whenerver we change the selected category 
+  },[searchTerm]);//recall the fetchFromApi function whenerver we change the search term
 
 
   return (

14b080ec69a45bff6506dec549a6f0e2de86b714
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-02 22:49:37 +0200

    add the handlesubmit function into searchbar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,11 +5,17 @@ import { Paper, IconButton } from '@mui/material';
 import {Search} from '@mui/icons-material';
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
   return (
     // paper is a simple div has a white background and some elevation ,it appears like it's floating on the top
     <Paper
         component="form"
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         sx={{
             borderRadius: 20,
             border: '1px solid #e3e3e3',
@@ -20,9 +26,10 @@ const SearchBar = () => {
     >
         <input 
             className="search-bar"
+            placeHolder="Search..."
             type="text" 
-            value=""
-            onChange={() => {}}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
         />
         <IconButton 
             type="submit"

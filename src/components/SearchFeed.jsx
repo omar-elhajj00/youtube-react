659dbabcf64f92cb19f18db6d1d78e0951b68abf
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-02 22:31:20 +0200

    implement the seachFeed.jsx as feed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {Box, Typography} from '@mui/material';
+
+import { Videos} from './';
+import {fetchFromAPI} from '../utils/fetchFromAPI';
+
+
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([]);
+
+  // useEffect is a lifecycle hook in which the code in this function will only run when we change the second parameter 
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${}`)//all the string inside `` are called template string
+      .then((data) => {
+        console.log(data);
+        setVideos(data.items)
+      } ) //.then contain what will be result of the promise
+       
+  },[]);//recall the fetchFromApi function whenerver we change the selected category 
+
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
+            {selectedCategory} <span style={{color: '#f31503'}}>videos</span>
+        </Typography>
+        <Videos videos={videos}/>
+    </Box>  
   )
 }
 
-export default SearchFeed
+export default Feed

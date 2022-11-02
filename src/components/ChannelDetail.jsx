4244c9e985d091b800e71da78eda9432d930a48d
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-02 14:46:15 +0200

    add the two functions of feth from api into channeldetails

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -9,14 +9,18 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 const ChannelDetail = () => {
 
   const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
 
   //how do we get access to that id from inside of our code, that's exactly what we use the useParams for
   const { id } = useParams();
 
   //useEffect is going to render as soon as our component opens
   useEffect(() => {
-    fetchFromAPI(`channels?part=snippet$id=${id}`)
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
       .then((data) => setChannelDetail(data?.items[0]));
+    //instead of channel we want to set the search and then channel id
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=data`)
+      .then((data) => setVideos(data?.items));
 
   }, [id]); //useEffect will rerender whenever the id changes 
 

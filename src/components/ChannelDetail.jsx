ff47002ecedcc4f629439bf1d391d8f9f7672f22
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-02 17:06:27 +0200

    design the videos inside channelDetail.jss

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -26,7 +26,32 @@ const ChannelDetail = () => {
 
 
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(0,255,255,1) 0%, rgba(255,0,245,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+          height: '300px'
+
+        }}
+          />
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+
+      <Box
+        display="flex"
+        p="2" 
+        >
+          <Box 
+            sx={{
+              mr: { sm: '100px' }
+            }} />
+              <Videos videos={videos} />
+
+          
+      </Box>
+
+    </Box>
   )
 }
 

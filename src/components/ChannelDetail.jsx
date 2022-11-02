b3dc918da81e2da7a08d612e21dbf1f3be63ff35
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-02 14:31:41 +0200

    add the useStatehook and fetchapi function inside channeldetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
 
 const ChannelDetail = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null);
+
+  //how do we get access to that id from inside of our code, that's exactly what we use the useParams for
+  const { id } = useParams();
+
+  //useEffect is going to render as soon as our component opens
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet$id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+  }, [id]); //useEffect will rerender whenever the id changes 
+
+
   return (
     <div>ChannelDetail</div>
   )
 }
 
-export default ChannelDetail
+export default ChannelDetail 

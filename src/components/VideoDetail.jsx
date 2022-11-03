abb71265dd2628a213db0138abe2525f3cc5de95
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-03 11:34:41 +0200

    import the main components into videoDetail component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { ImportantDevices } from '@mui/icons-material';
+import { Typography, Stack, Box } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
 
+import {Video} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 const VideoDetail = () => {
   return (
     <div>VideoDetail</div>

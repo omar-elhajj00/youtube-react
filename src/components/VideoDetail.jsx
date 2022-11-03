b780bec4b0b266ec0e9004964e2dc0724ff7fc87
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-03 11:42:56 +0200

    add the fetchfromapi function into useEffect hook

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { ImportantDevices } from '@mui/icons-material';
 import { Typography, Stack, Box } from '@mui/material';
@@ -7,9 +7,28 @@ import { CheckCircle } from '@mui/icons-material';
 
 import {Video} from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
+
 const VideoDetail = () => {
+
+  const [videoDetail, setVideoDetail] = useState(null);
+  const {id} =useParams();
+
+  useEffect(()=>{
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data)=> )
+  },[id]);
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+            <ReactPlayer />
+
+          </Box>
+        </Box>
+      </Stack>      
+    </Box>
   )
 }
 
